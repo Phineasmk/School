cab494d5b820ec8e6cfa048b600692459b9b6a90
Phineasmk <phineasmk@gmail.com>
2023-07-10 23:00:38 +0800

    A3 v3

--- Assignment/A3_7884874.py
+++ Assignment/A3_7884874.py
@@ -1,25 +1,20 @@
 import random
-from typing import Tuple
+import math
 
 # Generate 5-digit prime numbers p and q
-def generate_primes() -> Tuple[int, int]:
-    primes = []
-    for i in range(10000, 100000):
-        is_prime = True
-        for j in range(2, int(i ** 0.5) + 1):
-            if i % j == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(i)
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, number // 2 + 1):
+        if number% i == 0:
+            return False
+    return True
 
-    p = random.choice(primes)
-    q = random.choice(primes)
-    while p == q:
-        q = random.choice(primes)
-
-    return p, q
-
+def generate_primes(min_val, max_val):
+    prime = random.randint(min_val, max_val)
+    while not is_prime(prime):
+        prime = random.randint(min_val, max_val)
+    return prime
 
 # Calculate greatest common divisor of two numbers
 def gcd(a: int, b: int) -> int:
@@ -29,71 +24,46 @@ def gcd(a: int, b: int) -> int:
 
 
 # Calculate modular inverse of a number
-def mod_inv(a: int, m: int) -> int:
-    for x in range(1, m):
-        if (a * x) % m == 1:
-            return x
-    return None
+def mod_inv(e, phi):
+    for d in range(3, phi):
+        if (d * e) % phi == 1:
+            return d
+    raise ValueError('No modular inverse found')
 
+p, q = generate_primes(10000,100000), generate_primes(10000,100000)
 
+while p == q:
+    q = generate_primes(10000,100000)
+    
+n = p * q
 # Calculate Euler's totient function of n
-def euler_totient(p: int, q: int) -> int:
-    return (p - 1) * (q - 1)
+phi_n = (p-1) * (q-1)
+
+e = random.randint(3, phi_n - 1)
+while math.gcd (e, phi_n) != 1:
+    e = random.randint(3, phi_n - 1)
 
 
-# Generate public and private keys
-def generate_keys() -> Tuple[Tuple[int, int], Tuple[int, int]]:
-    p, q = generate_primes()
-    n = p * q
-    phi_n = euler_totient(p, q)
+d = mod_inv(e, phi_n)
 
-    # Choose a random integer e such that 1 < e < phi_n and gcd(e, phi_n) = 1
-    e = random.randint(2, phi_n - 1)
-    while gcd(e, phi_n) != 1:
-        e = random.randint(2, phi_n - 1)
-
-    # Calculate the modular inverse of e modulo phi_n
-    d = mod_inv(e, phi_n)
-
-    # Return public and private keys
-    public_key = (e, n)
-    private_key = (d, n)
-    return public_key, private_key
+print("Public key: ", e)
+print("Private key: ", d)
+print("n: ", n)
+print("Phi of n: ", phi_n)
+print("p: ", p)
+print("q: ", q)
 
 
-# Encrypt a message using public key
-def encrypt(public_key: Tuple[int, int], message: str) -> int:
-    e, n = public_key
+message = 'Hello World'
+
+message_encode = [ord(ch) for ch in message]
     
+# (m ^ e)mod n = c
+ciphertext =[pow(ch,e,n) for ch in message_encode]
 
+print(ciphertext)
 
-# Decrypt a ciphertext using private key
-def decrypt(private_key: Tuple[int, int], ciphertext: int) -> str:
-    d, n = private_key
-    m = pow(ciphertext, d, n)
-    message = ''
-    while m > 0:
-        message = chr(m & 0x7f) + message
-        m >>= 7
-    return message
+message_decode = [chr(pow(ch,d,n)) for ch in ciphertext]
+decrypted = "".join(chr(ch) for ch in message_decode)
 
-# Test the RSA algorithm with random data
-if __name__ == '__main__':
-    # Generate public and private keys
-    public_key, private_key = generate_keys()
-    print('Public key:', public_key)
-    print('Private key:', private_key)
-
-    # Test encryption and decryption
-    message = 'Hello World'
-    message_encode = [ord(ch) for ch in message]
-    
-    # (m ^ e)mod n = c
-    ciphertext =[pow(ch,e,n) for ch in message_encode for e, n in public_key ]
-    print(ciphertext)
-
-    # print('Original message:', message)
-    # ciphertext = encrypt(public_key, message)
-    # print('Ciphertext:', ciphertext)
-    # decrypted_message = decrypt(private_key, ciphertext)
-    # print('Decrypted message:', decrypted_message)
+print(decrypted)
